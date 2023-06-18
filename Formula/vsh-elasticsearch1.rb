71474fbd28e2a99f1804207f3bad727f62113f0f
Noah Mayr <dev@noahmayr.com>
2023-06-18 22:52:25 +0200

    Try alternative cask dep structure

--- Formula/vsh-elasticsearch1.rb
+++ Formula/vsh-elasticsearch1.rb
@@ -11,7 +11,7 @@ class VshElasticsearch1 < Formula
     sha256 big_sur: "e8a0ff9e463f14d857ea584bee4b0ade0799dd4131e7cd05782706df7a18ecb3"
   end
 
-  depends_on "homebrew/cask-versions/zulu8" => :cask
+  depends_on :cask => "homebrew/cask-versions/zulu8"
 
   def cluster_name
     "elasticsearch_#{ENV["USER"]}"
