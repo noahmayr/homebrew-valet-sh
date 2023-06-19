c6f3f87d8d2c8aad8d06215127580a3413aa8f7e
Noah Mayr <dev@noahmayr.com>
2023-06-19 22:09:06 +0200

    debug

--- Formula/vsh-elasticsearch1.rb
+++ Formula/vsh-elasticsearch1.rb
@@ -26,6 +26,7 @@ class VshElasticsearch1 < Formula
   def install
     java_home = Hardware::CPU.arm? ? `/usr/libexec/java_home -v 1.8 -a arm64 -F 2>/dev/null || /usr/libexec/java_home -v 1.8 -F` : "#{Formula['openjdk@8'].opt_libexec}/openjdk.jdk/Contents/Home"
 
+    puts "java_home:", java_home
     # Remove Windows files
     rm_f Dir["bin/*.bat"]
     rm_f Dir["bin/*.exe"]
